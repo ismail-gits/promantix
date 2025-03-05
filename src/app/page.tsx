cbf98bd4325eb528bcb0ef2697fe791f71c714c8
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-06 01:39:17 +0530

    updated input and select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div>
-      <Button size={"lg"}>Primary</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"muted"}>Muted</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"teritary"}>Teritary</Button>
+      <Input/>
     </div>
   )
 }

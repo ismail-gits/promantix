6c9184215c244c6e6e9e3c7d5cbfc1d2824d6a24
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-06 04:37:34 +0530

    updated SignInCard component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,16 +10,16 @@ const buttonVariants = cva(
     variants: {
       variant: {
         primary:
-          "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
+          "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700 cursor-pointer",
         destructive:
-          "bg-gradient-to-b from-amber-600 to-amber-700 text-primary-foreground hover:from-amber-700 hover:to-amber-700",
+          "bg-gradient-to-b from-amber-600 to-amber-700 text-primary-foreground hover:from-amber-700 hover:to-amber-700 cursor-pointer",
         outline:
-          "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground cursor-pointer",
         secondary:
-          "bg-white text-black hover:bg-neutral-100",
-        ghost: "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground",
-        muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80",
-        teritary: "bg-blue-100 text-blue-600 border-transparent shadow-none hover:bg-blue-200"
+          "bg-white text-black hover:bg-neutral-100 cursor-pointer",
+        ghost: "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground cursor-pointer",
+        muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80 cursor-pointer",
+        teritary: "bg-blue-100 text-blue-600 border-transparent shadow-none hover:bg-blue-200 cursor-pointer"
       },
       size: {
         default: "h-10 px-4 py-2 has-[>svg]:px-3",
--- src/features/auth/signInCard.tsx
+++ src/features/auth/signInCard.tsx
@@ -1,11 +1,18 @@
-import { DottedSeparator } from "@/components/dotted-separator"
-import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+"use client"
 
+import { DottedSeparator } from "@/components/dotted-separator"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
 
 export const SignInCard = () => {
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword ] = useState("")
+
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
-      <CardHeader className="flex items-center justify-center text-center p-7">
+      <CardHeader className="flex items-center justify-center text-center">
         <CardTitle className="text-2xl">
           Welcome Back!
         </CardTitle>
@@ -13,6 +20,60 @@ export const SignInCard = () => {
       <div className="px-7 mb-2">
         <DottedSeparator/>
       </div>
+      <CardContent>
+        <form className="space-y-4">
+          <Input
+             required
+             type="email"
+             value={email}
+             onChange={(e) => {
+              setEmail(e.target.value)
+             }}
+             placeholder="Enter your email"
+             disabled={false}
+          />
+          <Input
+             required
+             type="password"
+             value={password}
+             onChange={(e) => {
+              setPassword(e.target.value)
+             }}
+             placeholder="Enter your password"
+             disabled={false}
+             min={8}
+             max={256}
+          />
+          <Button
+            disabled={false}
+            size="lg"
+            className="w-full"
+          >
+            Login
+          </Button>
+        </form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator/>
+      </div>
+      <CardContent className="p-7 flex flex-col space-y-4">
+        <Button
+          variant={"secondary"}
+          disabled={false}
+          size={"lg"}
+          className="w-full"
+        >
+          Login with Google
+        </Button>
+        <Button
+          variant={"secondary"}
+          disabled={false}
+          size={"lg"}
+          className="w-full"
+        >
+          Login with Github
+        </Button>
+      </CardContent>
     </Card>
   )
 }

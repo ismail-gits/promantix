2776b1142c8c66f4658e1c703a03fa94378e2f1a
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-06 01:34:08 +0530

    update button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant={"default"} size="lg">Click me</Button>
-      <p className="text-red-500 font-semibold">
-        Ismail
-      </p>
+      <Button size={"lg"}>Primary</Button>
+      <Button variant={"secondary"}>Secondary</Button>
+      <Button variant={"destructive"}>Destructive</Button>
+      <Button variant={"ghost"}>Ghost</Button>
+      <Button variant={"muted"}>Muted</Button>
+      <Button variant={"outline"}>Outline</Button>
+      <Button variant={"teritary"}>Teritary</Button>
     </div>
   )
 }

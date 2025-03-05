d2a0a72e99fe583526e7ae012d1fe31ed444c5cb
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-06 00:33:15 +0530

    added required components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      Home Page
+      <Button variant={"default"} size="lg">Click me</Button>
+      <p className="text-red-500 font-semibold">
+        Ismail
+      </p>
     </div>
   )
 }

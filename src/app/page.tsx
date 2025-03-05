37b19610a736d37a9a3ba953ecb78f7e436c0321
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-06 03:16:53 +0530

    added (auth) route group and updated layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <div>
       <Input/>
+      <Button size={"lg"}>Primary</Button>
+      <Button variant={"secondary"}>Secondary</Button>
+      <Button variant={"destructive"}>Destructive</Button>
+      <Button variant={"ghost"}>Ghost</Button>
+      <Button variant={"muted"}>Muted</Button>
+      <Button variant={"outline"}>Outline</Button>
+      <Button variant={"teritary"}>Teritary</Button>
     </div>
   )
 }

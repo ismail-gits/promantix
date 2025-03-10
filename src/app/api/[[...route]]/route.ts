5bf305e4fe3bbd996c406b69775d03515a4d892a
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-11 01:44:54 +0530

    added libs/rpc.ts

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,4 +3,11 @@ import { handle } from "hono/vercel"
 
 const app = new Hono().basePath('/api')
 
+import auth from "@/features/auth/server/route"
+
+const routes = app
+  .route('/auth', auth)
+
 export const GET = handle(app)
+
+export type AppType = typeof routes

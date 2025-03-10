390a5481d8572b317a42ffb625f986e48598b057
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-11 02:19:24 +0530

    added useLogin.ts

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator"
-import { signUpSchema } from "../schemas";
+import { signInSchema, signUpSchema } from "../schemas";
 
 const app = new Hono()
-  .post('/signup', zValidator("json", signUpSchema), (c) => {
+  .post('/login', zValidator("json", signInSchema), (c) => {
     return c.json({Success: "Ok"})
   })
 

80f9b4a2ca1cd419cbe70b1876c052626a682236
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-13 22:11:27 +0530

    added appwrite.ts

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
 
-const app = new Hono().basePath('/api')
-
 import auth from "@/features/auth/server/route"
+
+const app = new Hono().basePath('/api')
 
 const routes = app
   .route('/auth', auth)

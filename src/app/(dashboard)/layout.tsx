63e973efc09a27358c0d37e1221288fcc095d7e9
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-23 07:45:25 +0530

    added Sidebar.tsx component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from "@/components/Sidebar"
 
 interface dashboardLayoutProps {
   children: React.ReactNode
@@ -5,8 +6,20 @@ interface dashboardLayoutProps {
 
 export default function DashboardLayout({ children }: dashboardLayoutProps) {
   return (
-    <div>
-      {children}
+    <div className="min-h-screen">
+      <div className="flex w-full h-full">
+        <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
+          <Sidebar/>
+        </div>
+        <div className="lg:pl-[264px]">
+          <div className="mx-auto max-w-screen-2xl h-4">
+            {/* TODO: NavBar */}
+            <main className="h-full py-8 px-6 flex flex-col">
+              {children} 
+            </main>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

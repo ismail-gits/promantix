f3d7875ead2c3593502bca2c59cef344d256cc7b
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-03-24 13:24:33 +0530

    updated button ui component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         primary:
-          "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700 cursor-pointer",
+          "bg-gradient-to-b from-gray-800 to-gray-900 text-primary-foreground hover:from-gray-900 hover:to-black cursor-pointer",
         destructive:
           "bg-gradient-to-b from-amber-600 to-amber-700 text-primary-foreground hover:from-amber-700 hover:to-amber-700 cursor-pointer",
         outline:
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-white text-black hover:bg-neutral-100 cursor-pointer",
         ghost: "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground cursor-pointer",
         muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80 cursor-pointer",
-        teritary: "bg-blue-100 text-blue-600 border-transparent shadow-none hover:bg-blue-200 cursor-pointer"
+        teritary: "bg-gray-100 text-gray-600 border-transparent shadow-none hover:bg-gray-200 cursor-pointer"
       },
       size: {
         default: "h-10 px-4 py-2 has-[>svg]:px-3",
